2667134505058951f56520fb675e3244e79f7924
Shiki42 <hushuyuan42@gmail.com>
2026-01-04 18:43:45 +0000

    Deploying to gh-pages from @ Shiki42/Shiki42.github.io@dea107a36623b320ec7fcb0742650fa356ced5f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -369,8 +369,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-i-graduated-from-the-university-of-southern-california",
+          title: 'I graduated from the University of Southern California! 🎉',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
