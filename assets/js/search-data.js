de687533e6abbcf48b44d111df336e75d6747521
Shiki42 <hushuyuan42@gmail.com>
2026-01-04 19:05:57 +0000

    Deploying to gh-pages from @ Shiki42/Shiki42.github.io@0000561498a61c3e869efe7f169ab87a773bc8a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,14 +422,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%68%75%73%68%75%79%75%61%6E%34%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -443,14 +436,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=zXVrlqMAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
